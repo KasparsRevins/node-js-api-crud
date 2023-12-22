560677cf28f84105a36cc83aa8336d04bbd28a45
Kaspars Revins <okoikwedced@gmail.com>
2023-12-22 11:18:26 +0000

    Readded sign up functionality

--- router/post.js
+++ router/post.js
@@ -10,6 +10,37 @@ router.use(express.urlencoded({ extended: false }));
 
 // Signup route
 router.post("/signup", async (req, res) => {
+  const { username, surname, email, password } = req.body;
+
+  try {
+    // Check if user already exists in the database
+    const existingUserQuery = "SELECT * FROM tester WHERE email = $1";
+    const existingUserResult = await pool.query(existingUserQuery, [email]);
+    if (existingUserResult.rows.length > 0) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Hash the password before storing it in the database
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const insertUserQuery =
+      "INSERT INTO tester (username, surname,email, password) VALUES ($1, $2, $3, $4) RETURNING *";
+    const newUserResult = await pool.query(insertUserQuery, [
+      username,
+      surname,
+      email,
+      hashedPassword,
+    ]);
+
+    const newUser = newUserResult.rows[0];
+    res
+      .status(201)
+      .json({ message: "User created successfully", user: newUser });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error creating user", error: error.message });
+  }
 });
 
 // Login route
@@ -34,11 +65,15 @@ router.post("/signin", async (req, res) => {
     }
 
     // Generate a JWT token for the authenticated user
-    const token = jwt.sign({ userId: userResult.rows[0].id }, "your-secret-key", {
-      expiresIn: "1h", // Token expiration time
-    });
+    const token = jwt.sign(
+      { userId: userResult.rows[0].id },
+      "your-secret-key",
+      {
+        expiresIn: "1h", // Token expiration time
+      }
+    );
 
-    res.status(200).json({ message: "Login successful", token, redirect: "/"  });
+    res.status(200).json({ message: "Login successful", token, redirect: "/" });
   } catch (error) {
     res.status(500).json({ message: "Error logging in", error: error.message });
   }
